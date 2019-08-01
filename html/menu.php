b469fdef84fea9ec13cbf8df28b4aec251f6e5f6
victorhugofc <32600701+victorhugofc@users.noreply.github.com>
2019-08-01 11:57:59 -0300

    Add files via upload

---
+++ html/menu.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <div class="a p-0 mb-0" style="position:absolute;top:0px;left:0px; width: 100%; background-color:#f0f2f4;">
+       <p class="" style= > </p>
+           <div class="menu">
+             <div class="logo">
+               <a class="nav-link" href="..\html\index.php"><img src="..\img\logo.png" alt="" width="200" height="75" ></a>
+             </div>
+                 <ul class="nav justify-content-end">
+                   <li class="nav-item">
+                     <a class="nav-link active" href="index.php"><img src="https://img.icons8.com/ios/25/000000/home.png"> Home</a>
+                   </li>
+             <li class="nav-item">
+               <a class="nav-link active" href="cadastrar.html"><img src="https://img.icons8.com/ios/25/000000/add-user-group-man-man.png"> Cadastrar</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#"><img src="https://img.icons8.com/ios/25/000000/enter-2.png"> Login</a>
+             </li>
+             <li class="nav-item">
+               <a class="nav-link" href="#"> <img src="https://img.icons8.com/ios/25/000000/new-post.png"> SAC</a>
+             </li>
+           </ul> </div>
+    </div>  </div>
+  </body>
+
+
+
+  <div class="b">
+
+      <div class=" d-flex flex-justify-between pt-0 pb-0 mt-0  text-gray border-gray-light "style="position:absolute;bottom:100%;left:0px; width: 100%; >
+        <ul class="">
+          <li class="mr-3">© 2019 <span title="0.38096s from unicorn-6bc65b88c6-8xqfm">BuscaEmprego</span>, Inc.</li>
+        </ul>
+    </a>
+       <ul class="list-style-none d-flex flex-wrap ">
+         <li class="mr-3"><a data-ga-click="Footer, go to terms, text:terms" href="https://github.com/site/terms">Termos</a></li>
+         <li class="mr-3"><a data-ga-click="Footer, go to privacy, text:privacy" href="https://github.com/site/privacy">Privacidade</a></li>
+         <li class="mr-3"><a data-ga-click="Footer, go to security, text:security" href="https://github.com/security">Segurança</a></li>
+         <li><a data-ga-click="Footer, go to help, text:help" href="https://help.github.com">Ajuda</a></li>
+  </div>
+</html>
